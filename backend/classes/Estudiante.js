1f1fe77c23fce7db34320c625941123b1ce272f0
Fekazo <xdggomar@gmail.com>
2024-09-26 03:12:14 -0500

    creacion de clases
    primeras clases creadas

---
+++ backend/classes/Estudiante.js
@@ -0,0 +1,12 @@
+class Estudiante {
+    constructor(idestudiante, idusuario, programa, semestre, promedio) {
+      this.idestudiante = idestudiante;
+      this.idusuario = idusuario;  
+      this.programa = programa;
+      this.semestre = semestre;
+      this.promedio = promedio;
+    }
+  }
+  
+  export default Estudiante;
+  
